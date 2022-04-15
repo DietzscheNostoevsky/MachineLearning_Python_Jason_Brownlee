65c5e966935a269dc84277342b91bcaf9f4ead9b
DietzscheNostoevsky <96907475+DietzscheNostoevsky@users.noreply.github.com>
2022-04-16 02:56:59 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Probability_for_ML_Python_Jason_Brownlee
+
+## Learning Probability for Machine Learning using Python
+
  
---
+++ versions.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+# check library version numbers
+
+import os
+import sklearn
+import statsmodels
+import pandas
+import matplotlib
+import numpy
+import scipy
+
+os.system("clear")
+# scipy
+print('scipy: %s' % scipy.__version__)
+# numpy
+print('numpy: %s' % numpy.__version__)
+# matplotlib
+print('matplotlib: %s' % matplotlib.__version__)
+# pandas
+print('pandas: %s' % pandas.__version__)
+# statsmodels
+print('statsmodels: %s' % statsmodels.__version__)
+# scikit-learn
+print('sklearn: %s' % sklearn.__version__)
