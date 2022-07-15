0d4648b0d0f3174f8f8f3ef5619fff3d0c774195
DietzscheNostoevsky <96907475+DietzscheNostoevsky@users.noreply.github.com>
2022-07-15 23:36:49 +0530

    coorected indent

--- Probability_Python/listing_6_1.py
+++ Probability_Python/listing_6_1.py
@@ -6,6 +6,6 @@ days = 365
 # calculate probability for different group sizes
 p = 1.0
 for i in range(1, n):
-av = days - i
-p *= av / days
-print('n=%d, %d/%d, p=%.3f 1-p=%.3f' % (i+1, av, days, p*100, (1-p)*100))
+    av = days - i
+    p *= av / days
+    print('n=%d, %d/%d, p=%.3f 1-p=%.3f' % (i+1, av, days, p*100, (1-p)*100))
