0c74034462982a2b53257ea5bfddb018b89457ae
DietzscheNostoevsky <96907475+DietzscheNostoevsky@users.noreply.github.com>
2022-07-21 23:45:47 +0530

    added code

---
+++ Probability_Python/listing_8_9.py
@@ -0,0 +1,13 @@
+
+# example of simulating a multinomial process
+from numpy.random import multinomial
+# define the parameters of the distribution
+p = [1.0/3.0, 1.0/3.0, 1.0/3.0]
+k = 100
+
+8.7. Further Reading 57
+# run a single simulation
+cases = multinomial(k, p)
+# summarize cases
+for i in range(len(cases)):
+print('Case %d: %d' % (i+1, cases[i]))
