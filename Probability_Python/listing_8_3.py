68b12027c8cd36115069023eb128e524ad1ac80d
DietzscheNostoevsky <96907475+DietzscheNostoevsky@users.noreply.github.com>
2022-08-13 23:52:04 +0530

    Update listing_8_3.py

--- Probability_Python/listing_8_3.py
+++ Probability_Python/listing_8_3.py
@@ -4,5 +4,5 @@ from scipy.stats import binom
 # define the parameters of the distribution
 p = 0.3
 k = 100
-# calculate moments
+# calculate moments #
 mean, var, _, _ = binom.stats(k, p, moments='mvsk') print('Mean=%.3f, Variance=%.3f' % (mean, var))
